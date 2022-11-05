fd2d3a846fac09c4620e5639f2dee957b806e399
HuuTaiOne <nguyentaint979@gmail.com>
2022-11-05 16:40:54 +0700

    tao ra nhanh 1

---
+++ HocGit1/src/nhanh1/hello_2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nhanh1;
+
+/**
+ *
+ * @author MSI PC
+ */
+public class hello_2 {
+    public static void main(String[] args) {
+        System.out.println("nhanh1.hello_2.main");
+    }
+}
