d9fd1eddd593026efee586b8ee0cae71327e6214
KhanhTran <trantankhanh31102003@gmail.com>
2022-11-05 19:37:28 +0700

    Khanh - da them code vao

---
+++ HocGit1/src/nhanh2/Helloo3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nhanh2;
+
+/**
+ *
+ * @author asus
+ */
+public class Helloo3 {
+    public static void main(String[] args) {
+        System.out.println("100111100000000111");
+    }
+}
